3e81054019006a745dfb5ec9f7e448ef8df2b29c
SiegfriedWagner <mateus.chojnowski@gmail.com>
2019-02-15 13:51:32 +0100

    relase

--- libmatrix/matrix.hpp
+++ libmatrix/matrix.hpp
@@ -7,34 +7,45 @@
 #include <sstream>
 #include <string.h>
 #include <exception>
-//TODO: Replace for loop with tasty std::iterators
+#include <type_traits>
 
-template <class Type> class Matrix {
+template <class Type>
+class Matrix {
 protected:
   size_t rows;
   size_t columns;
   size_t size;
   Type *value;
   void resize(size_t columns, size_t rows);
-  inline bool dimEqualTo(const Matrix<Type> &other) const;
+  inline Type *_begin() { return value; }
+  inline Type *_end() { return value+size; }
 public:
+  inline const Type * begin() const { return value; }
+  inline const Type * end() const { return value+size; }
+  inline bool dimEqualTo(const Matrix<Type> &other) const
+  { return columns == other.columns && rows == other.rows; }
+  inline size_t GetRows() const { return rows; }
+  inline size_t GetColumns() const { return columns; }
   Matrix();
   Matrix(size_t columns, size_t rows);
   Matrix(size_t columns, size_t rows, const Type *value);
-  Matrix(size_t columns, size_t rows, const Type **value);
+  Matrix(size_t columns, size_t rows, Type **to_move); //Not working
   Matrix(std::initializer_list<std::initializer_list<Type>> value);
   Matrix(const Matrix &to_copy);
   Matrix(Matrix &&to_move);
   ~Matrix();
+  template <typename X> operator Matrix<X>() const;
   Matrix<Type> &operator=(const Matrix<Type> &other);
   Matrix<Type> operator+(const Matrix<Type> &other);
+  // EXPERIMENTAL: OPERATION WORKS WITH DIFERENT TYPES OF MATRIX
+  // template <typename X>  Matrix<Type> operator+(Matrix<X> &other);
   Matrix<Type> operator-(const Matrix<Type> &other);
   Matrix<Type> operator*(const Matrix<Type> &other);
   Matrix<Type> operator%(const Matrix<Type> &other);
   Matrix<Type> &operator+=(const Matrix<Type> &other);
   Matrix<Type> &operator-=(const Matrix<Type> &other);
   Matrix<Type> &operator*=(const Matrix<Type> &other);
-  Matrix<Type> &operator%=(const Matrix<Type> &other);
+  Matrix<Type> &operator/=(const Matrix<Type> &other);
   bool operator==(const Matrix<Type> &other) const;
   bool operator!=(const Matrix<Type> &other);
   Matrix<Type> operator+(const Type &var);
@@ -48,7 +59,26 @@ public:
   Type &operator()(const size_t row, const size_t column);
   Type operator()(const size_t row, const size_t columen) const;
   Matrix<Type> T();
-  friend std::ostream &operator<<(std::ostream &o, const Matrix &matrix) {
+
+  friend Matrix operator+(Type var, Matrix<Type> matrix) {
+    return matrix+var;
+  };
+  
+  friend Matrix<Type> operator-(Type var, Matrix<Type> &matrix) {
+    //TODO: Consider more optimal solution
+    return matrix*(-1)+var;
+  }
+
+  friend Matrix<Type> operator*(Type var, Matrix<Type> &matrix) {
+    return matrix*var;
+  }
+  
+  template <typename X> inline bool dimEqualTo(const Matrix<X> &other) {
+    return this->GetColumns() == other.GetColumns() && this->GetRows() == other.GetRows();
+  }
+
+  
+  friend std::ostream &operator<<(std::ostream &o, const Matrix<Type> &matrix) {
     o << "[";
     for (int r = 0; r < matrix.rows; ++r) {
       if (!r)
@@ -66,15 +96,17 @@ public:
     o << "]" << std::endl;
     return o;
   }
-  friend std::ofstream &operator<<(std::ofstream &o, const Matrix &matrix) {
+
+  friend std::ofstream &operator<<(std::ofstream &o, Matrix<Type> &matrix) {
     o << matrix.columns << std::endl;
     o << matrix.rows << std::endl;
-    for (int i=0; i < matrix.size; ++i) {
-      o << *(matrix.value+i) << " ";
+    for (auto it=matrix.begin(); it < matrix.end(); ++it) {
+      o << *(it) << " ";
     }
     return o;
   }
-  friend std::ifstream &operator>>(std::ifstream &o, Matrix &matrix) {
+
+  friend  std::ifstream &operator>>(std::ifstream &o, Matrix<Type> &matrix) {
     std::string bufferString;
     int columns, rows;
     getline(o, bufferString);
@@ -83,36 +115,23 @@ public:
     rows = stoi(bufferString);
     getline(o, bufferString);
     std::stringstream bufferStream(bufferString);
-    if (columns*rows != matrix.size)
+    if (columns != matrix.columns || rows != matrix.rows)
       matrix.resize(columns, rows);
-    for (int i=0; i< matrix.size; ++i) {
+    for (auto it= matrix.begin(); it < matrix.end(); ++it) {
       bufferStream >> bufferString;
-      matrix.value[i] = stod(bufferString); // TODO: Think about better option to implement that
+      *(it) = stod(bufferString); // TODO: Think about better option to implement that
     }
     return o;
   }
-  
-  friend Matrix<Type> operator+(Type var, Matrix<Type> &matrix) {
-    return matrix+var;
-  }
-
-  friend Matrix<Type> operator-(Type var, Matrix<Type> &matrix) {
-    //TODO: Consider more optimal solution
-    return matrix*(-1)+var;
-  }
-  friend Matrix<Type> operator*(Type var, Matrix<Type> &matrix) {
-    return matrix*var;
-  }
-  friend Matrix<Type> operator+(Matrix &other, Matrix<Type> &that) {
-   if (!that.dimEqualTo(other)) 
-    throw std::invalid_argument("Dimensions of both matrixes are different");
-   Matrix ret = Matrix(that);
-   for (int i=0; i < that.size; ++i) {
-     ret.value[i]+= other.value[i];
-   }
-   return ret;
-  }
 };
+
+template <typename Type> void Matrix<Type>::resize(size_t columns, size_t rows) {
+  this->columns = columns;
+  this->rows = rows;
+  this->size = columns*rows;
+  delete [] value;
+  value = new Type[this->size];
+}
 
 template <typename Type> Matrix<Type>::Matrix(): size(0), rows(0), columns(0), value(nullptr) {}
 
@@ -132,18 +151,25 @@ template<typename Type> Matrix<Type>::Matrix(size_t columns, size_t rows, const 
 }
 
 template <typename Type>
+Matrix<Type>::Matrix(size_t columns, size_t rows, Type **to_move) { //TODO: Fix me: munmap_chunk(): invalid pointer
+
+  this->rows = rows;
+  this->columns = columns;
+  this->size = columns*rows;
+  this->value = *to_move;
+  *to_move = (Type *) nullptr;
+}
+
+template <typename Type>
 Matrix<Type>::Matrix(std::initializer_list<std::initializer_list<Type>> value) {
   this->rows = value.size();
   this->columns = value.begin()->size();
   this->size = this->columns * this->rows;
   this->value = new Type[size];
-  int iter = 0;
-  for (auto row = value.begin(); row < value.end(); ++row) {
-    for (auto col = (*row).begin(); col < (*row).end(); ++col) {
-      this->value[iter] = *col;
-      ++iter;
-    }
-  }
+  auto iter = this->_begin();
+  for (auto row = value.begin(); row < value.end(); ++row)
+    for (auto col = (*row).begin(); col < (*row).end(); ++col)
+      *(iter++) = *col;
 }
 
 template <typename Type> Matrix<Type>::Matrix(const Matrix &to_copy) {
@@ -155,42 +181,32 @@ template <typename Type> Matrix<Type>::Matrix(const Matrix &to_copy) {
 }
 
 template <typename Type> Matrix<Type>::Matrix(Matrix &&to_move) {
-    this->value = to_move.value;
-    this->columns = to_move.columns;
-    this->rows = to_move.rows;
-    this->size = to_move.size;
-    to_move.value = nullptr;
-    to_move.columns = 0;
-    to_move.rows = 0;
-    to_move.size = 0;
+  this->value = to_move.value;
+  this->columns = to_move.columns;
+  this->rows = to_move.rows;
+  this->size = to_move.size;
+  to_move.value = nullptr;
+  to_move.columns = 0;
+  to_move.rows = 0;
+  to_move.size = 0;
 }
 
 template <typename Type> Matrix<Type>::~Matrix() {
   delete[] this->value;
 }
 
-template <typename Type> void Matrix<Type>::resize(size_t columns, size_t rows) {
-  this->columns = columns;
-  this->rows = rows;
-  this->size = columns*rows;
-  delete [] value;
-  value = new Type[this->size];
-}
-
-template <typename Type> inline bool Matrix<Type>::dimEqualTo(const Matrix &other) const {
-  return this->columns == other.columns && this->rows == other.rows;
-}
-
-template <typename Type> Type &Matrix<Type>::operator()(size_t column, size_t row) {
-  if (column > this->columns-1 || row > this->rows-1)
-    throw std::out_of_range("Index out of range"); //TODO: Better explanation, format
-  return this->value[row*this->columns+column];
-}
-
-template <typename Type> Type Matrix<Type>::operator()(size_t column, size_t row) const {
-  if (column > this->columns-1 || row > this->rows-1)
-    throw std::out_of_range("Index out of range"); //TODO: Better explanation, format
-  return this->value[row*this->columns+column];
+template <typename Type> template <typename X> Matrix<Type>::operator Matrix<X>() const {
+  if (!std::is_convertible<Type, X>::value)
+    throw std::runtime_error("Conversion failed"); // TODO: Better explanation, format{
+  X tab[this->size];
+  X *tab_p = tab;
+  if (tab == nullptr)
+    throw std::bad_alloc();
+  for (auto it = this->begin(); it < this->end(); it++) {
+    *(tab_p++) = (Type) (*it);
+  }
+  Matrix<X> ret = Matrix<X>(this->columns, this->rows, tab);
+  return ret;
 }
 
 template <typename Type> Matrix<Type> &Matrix<Type>::operator=(const Matrix &other) {
@@ -202,8 +218,9 @@ template <typename Type> Matrix<Type> &Matrix<Type>::operator=(const Matrix &oth
     }
     this->columns = other.columns;
     this->rows = other.rows;
-    for (size_t i = 0; i < this->size; ++i)
-      this->value[i] = other.value[i];
+    auto o_iter = other.begin();
+    for (auto it = this->_begin(); it < this->end();)
+      *(it++) = *(o_iter++);
   }
   return *this;
 }
@@ -211,18 +228,30 @@ template <typename Type> Matrix<Type> Matrix<Type>::operator+(const Matrix &othe
   if (!this->dimEqualTo(other)) 
     throw std::invalid_argument("Dimensions of both matrixes are different");
   Matrix ret = Matrix(*this);
-  for (int i=0; i < this->size; ++i) {
-    ret.value[i]+= other.value[i];
+  auto o_iter = other.begin();
+  for (auto it=ret._begin(); it < ret.end();) {
+    *(it++)+=*(o_iter++);
   }
   return ret;
 }
+// template <typename Type> template <typename X>
+// Matrix<Type> Matrix<Type>::operator+(Matrix<X> &other) {
+//   if (!this->dimEqualTo(other)) 
+//     throw std::invalid_argument("Dimensions of both matrixes are different");
+//   Matrix ret = Matrix(*this);
+//   auto o_it = other.begin();
+//   for (auto it=ret._begin(); it < ret.end();)
+//     *(it++)+=*(o_it++);
+//   return ret;
+// }
 
 template <typename Type> Matrix<Type> Matrix<Type>::operator-(const Matrix &other) {
   if (!this->dimEqualTo(other)) 
     throw std::invalid_argument("Dimensions of both matrixes are different");
   Matrix ret = Matrix(*this);
-  for (int i=0; i < this->size; ++i)
-    ret.value[i]-= other.value[i];
+  auto o_it = other.begin();
+  for (auto it=ret._begin(); it < ret.end();)
+    *(it++)-= *(o_it++);
   return ret;
 }
 
@@ -230,20 +259,21 @@ template <typename Type> Matrix<Type> Matrix<Type>::operator*(const Matrix &othe
   if (!this->dimEqualTo(other))
     throw  std::invalid_argument("Dimensions of both matrixes are different");
   Matrix ret = Matrix<Type>(*this);
-  for (int i=0; i < this->size; ++i)
-    ret.value[i]*=other.value[i];
+  auto o_it = other.begin();
+  for (auto it = ret._begin(); it < ret.end();)
+    *(it++) = *(o_it++);
   return ret;
 }
 
 template <typename Type> Matrix<Type> Matrix<Type>::operator%(const Matrix &other) {
   if (this->columns != other.rows || this->rows != other.columns)
     throw  std::invalid_argument("Rows and columns does not equal"); // TOOD: Make msg better
-  Matrix ret = Matrix<Type>(other.rows, this->columns);
+  Matrix ret = Matrix<Type>(this->rows, other.columns);
   for (size_t row=0; row < ret.rows; ++row) {
     for (size_t col=0; col < ret.columns; ++col) {
       Type val = Type();
-      for (size_t i =0; i < this->rows; ++i) {
-	val+=(*this)(row, i)*other(i, col);
+      for (size_t i =0; i < this->columns; ++i) {
+	val+=(*this)(i, row)*other(col, i);
       }
       ret(row, col)=val;
     }
@@ -253,8 +283,9 @@ template <typename Type> Matrix<Type> Matrix<Type>::operator%(const Matrix &othe
 
 template <typename Type> bool Matrix<Type>::operator==(const Matrix<Type> &other) const {
   if (this->dimEqualTo(other)) {
-    for (size_t i = 0; i < this->size; ++i)
-      if (this->value[i] != other.value[i])
+    auto o_it = other.begin();
+    for (auto it = this->begin(); it < this->end();)
+      if (*(it++) != *(o_it++))
 	return false;
     return true;
   }
@@ -268,56 +299,113 @@ template <typename Type> bool Matrix<Type>::operator!=(const Matrix<Type> &other
 
 template <typename Type> Matrix<Type> Matrix<Type>::operator+(const Type &var) {
   Matrix ret = Matrix(this->columns, this->rows);
-  for (int i =0; i < ret.size; ++i)
-    ret.value[i]=this->value[i]+var;
+  auto t_it = this->begin();
+  for (auto it = ret._begin(); it < ret.end();)
+    *(it++)=*(t_it++)+var;
   return ret;
 }
 
 template <typename Type> Matrix<Type> Matrix<Type>::operator-(const Type &var) {
   Matrix ret = Matrix(this->columns, this->rows);
-  for (int i =0; i < ret.size; ++i)
-    ret.value[i]=this->value[i]-var;
+  auto t_it = this->begin();
+  for (auto it = ret._begin(); it < ret.end();)
+    (*it++) = *(t_it++)-var;
   return ret;
 }
 
 template <typename Type> Matrix<Type> Matrix<Type>::operator*(const Type &var) {
   Matrix ret = Matrix(this->columns, this->rows);
-  for (int i =0; i < ret.size; ++i)
-    ret.value[i]=this->value[i]*var;
+  auto t_it = this->begin();
+  for (auto it = ret._begin(); it < ret.end();)
+    (*it++) = *(t_it++)*var;
   return ret;
 }
 
 template <typename Type> Matrix<Type> Matrix<Type>::operator/(const Type &var) {
-  // TODO: Divide by 0 exception here
+  if (var == 0)
+    throw std::overflow_error("Divide by zero");
   Matrix ret = Matrix(this->columns, this->rows);
-  for (int i =0; i < ret.size; ++i) 
-    ret.value[i]=this->value[i]/var;
+  auto t_it = this->begin();
+  for (auto it = ret._begin(); it < ret.end();)
+    (*it++) = *(t_it++)/var;
   return ret;
 }
 
+template <typename Type> Matrix<Type> &Matrix<Type>::operator+=(const Matrix<Type> &other) {
+  if (!this->dimEqualTo(other)) 
+    throw std::invalid_argument("Dimensions of both matrixes are different");
+  auto o_it = other.begin();
+  for (auto it = _begin(); it < end();)
+    *(it++)+=*(o_it++);
+  return *this;
+}
+
+template <typename Type> Matrix<Type> &Matrix<Type>::operator-=(const Matrix<Type> &other) {
+  if (!this->dimEqualTo(other)) 
+    throw std::invalid_argument("Dimensions of both matrixes are different");
+  auto o_it = other.begin();
+  for (auto it = _begin(); it < end();)
+    *(it++)-=*(o_it++);
+  return *this;
+}
+
+template <typename Type> Matrix<Type> &Matrix<Type>::operator*=(const Matrix<Type> &other) {
+  if (!this->dimEqualTo(other)) 
+    throw std::invalid_argument("Dimensions of both matrixes are different");
+  auto o_it = other.begin();
+  for (auto it = _begin(); it < end();)
+    *(it++)*=*(o_it++);
+  return *this;
+}
+
+template <typename Type> Matrix<Type> &Matrix<Type>::operator/=(const Matrix<Type> &other) {
+  if (!this->dimEqualTo(other)) 
+    throw std::invalid_argument("Dimensions of both matrixes are different");
+  auto o_it = other.begin();
+  for (auto it = _begin(); it < end();)
+    if (*(o_it) == 0)
+      throw std::overflow_error("Divide by zero");
+    else
+      *(it++)/=*(o_it++);
+  return *this;
+}
+
 template <typename Type> Matrix<Type> &Matrix<Type>::operator+=(const Type &var) {
-  for (int i =0; i < size; ++i)
-    value[i]+=var;
+  for (auto it = this->_begin(); it < this->end(); it++)
+    *it+=var;
   return *this;
 }
 
 template <typename Type> Matrix<Type> &Matrix<Type>::operator-=(const Type &var) {
-  for (int i =0; i < size; ++i)
-    value[i]-=var;
+  for (auto it = this->_begin(); it < this->end(); it++)
+    *it-=var;
   return *this;
 }
 
 template <typename Type> Matrix<Type> &Matrix<Type>::operator*=(const Type &var) {
-  for (int i =0; i < size; ++i)
-    value[i]*=var;
+  for (auto it = this->_begin(); it < this->end(); it++)
+    *it*=var;
   return *this;
 }
 
 template <typename Type> Matrix<Type> &Matrix<Type>::operator/=(const Type &var) {
-  //TODO: Zero divide exception
-  for (int i =0; i < size; ++i)
-    value[i]+=var;
+  if (var == 0)
+    throw std::overflow_error("Divide by zero");
+  for (auto it = this->_begin(); it < this->end(); it++)
+    (*it)/=var;
   return *this;
+}
+
+template <typename Type> Type &Matrix<Type>::operator()(size_t column, size_t row) {
+  if (column > this->columns-1 || row > this->rows-1)
+    throw std::out_of_range("Index out of range"); //TODO: Better explanation, format
+  return this->value[row*this->columns+column];
+}
+
+template <typename Type> Type Matrix<Type>::operator()(size_t column, size_t row) const {
+  if (column > this->columns-1 || row > this->rows-1)
+    throw std::out_of_range("Index out of range"); //TODO: Better explanation, format
+  return this->value[row*this->columns+column];
 }
 
 template <typename Type> Matrix<Type> Matrix<Type>::T() { //QUESTION: OPTIONAL INPLACE?
--- test/test.cpp
+++ test/test.cpp
@@ -1,25 +1,136 @@
 #include "gtest/gtest.h"
 #include "matrix.hpp" // TODO: Check if can be resolved better
+#include <exception>
 class MatrixIntTest : public ::testing::Test {
 public:
   Matrix<int> tested;
+  Matrix<int> zero;
 protected:
-  MatrixIntTest() { tested = Matrix<int>({{1, 2, 3}, {4,5,6}}); }
+  MatrixIntTest() { tested = Matrix<int>({{1, 2, 3}, {4,5,6}});
+    zero = Matrix<int>({{0, 0, 0}, {0,0,0}});
+      }
 };
 
-TEST(MatrixTest, ConstructorTest) {
+TEST(MatrixTest, TypeTest) {
   Matrix<int> testInt = Matrix<int>();
   Matrix<float> testFloat = Matrix<float>();
   Matrix<double> testDouble = Matrix<double>();
   Matrix<Matrix<int>> testMatrixInt = Matrix<Matrix<int>>();
 }
 
-TEST_F(MatrixIntTest, AddIntTest) {
-  Matrix<int> matrix = Matrix<int>({{2, 3, 4}, {5, 6, 7}});
+TEST(MatrixText, ConstructorTest) {
+  int tabInt[9] = {1,2,3,4,5,6};
+  Matrix<int> first = Matrix<int>(3,2, tabInt);
+  Matrix<int> second = Matrix<int>({{1, 2, 3}, {4, 5, 6}});
+  Matrix<int> third = Matrix<int>(first);
+  ASSERT_EQ(first, second);
+  ASSERT_EQ(first, third);
+}
+
+TEST_F(MatrixIntTest, AddIntTestOne) {
+  Matrix<int> matrix = Matrix<int>({{2, 3, 4}, {5, 6, 7}});  
   ASSERT_EQ(tested + 1, matrix);
+  ASSERT_EQ(1 + tested, matrix);
   tested+=1;
   ASSERT_EQ(tested, matrix);
 }
+
+TEST_F(MatrixIntTest, AddIntTestTwo) {
+ Matrix<int> twice = Matrix<int>({{2, 4, 6}, {8, 10, 12}});
+ ASSERT_EQ(tested+tested, twice);
+ tested+=tested;
+ ASSERT_EQ(tested, twice);
+}
+TEST_F(MatrixIntTest, SubtractTest) {
+  Matrix<int> matrix = Matrix<int>({{0, 1, 2}, {3, 4, 5}});
+  Matrix<int> mat = Matrix<int>({{0, -1, -2}, {-3, -4, -5}});
+  ASSERT_EQ(tested - 1, matrix);
+  ASSERT_EQ(1 - tested, mat);
+  tested-=1;
+  ASSERT_EQ(tested, matrix);
+  ASSERT_EQ(tested-tested, zero);
+  tested-=tested;
+  ASSERT_EQ(tested, zero);
+}
+
+TEST_F(MatrixIntTest, ElementWise) {
+  Matrix<int> _double = Matrix<int>({{2, 4, 6}, {8, 10, 12}});
+  Matrix<int> _minus = Matrix<int>({{-1, -2, -3}, {-4, -5, -6}});
+  Matrix<int> _pow = Matrix<int>({{1, 2, 9}, {16,25,36}});
+  Matrix<int> ones = Matrix<int>({{1, 1, 1}, {1,1,1}});
+  ASSERT_EQ(tested * 2, _double);
+  ASSERT_EQ(tested * -1, _minus);
+  tested*=2;
+  ASSERT_EQ(tested, _double);
+  tested*=ones;
+  ASSERT_EQ(tested, _double);
+}
+
+TEST_F(MatrixIntTest, DivideZeroTest) {
+  try {
+    tested/0;
+    FAIL() << "Expected std::overflow_error :1";
+  } catch (std::overflow_error const &err) {
+    EXPECT_EQ(err.what(), std::string("Divide by zero"));
+  }
+  catch (...) {
+    FAIL() << "Expected std::overflow_error :2";  
+  }
+  try {
+    tested/=0;
+    FAIL() << "Expected std::overflow_error :3";
+  }
+  catch (std::overflow_error const &err) {
+    EXPECT_EQ(err.what(), std::string("Divide by zero"));
+  }
+  catch (...) {
+    FAIL() << "Expected std::overflow_error :4";  
+  }
+}
+
+TEST_F(MatrixIntTest, DivideTest) {
+  Matrix<int> divide_two = Matrix<int>({{0, 1, 1}, {2, 2, 3}});
+  ASSERT_EQ(tested / 2, divide_two);
+  ASSERT_EQ(tested/=2, divide_two);
+}
+
+TEST_F(MatrixIntTest, CastTest) {
+  Matrix<double> _double = (Matrix<double>) tested;
+  Matrix<float> _float = (Matrix<float>) tested;
+  ASSERT_EQ(_double, tested);
+  ASSERT_EQ(_float, tested);
+  Matrix<double> _double_s = Matrix<double>({{1.2, 2.4, 3.7}, {4.9, 5.8, 6.9}});
+  ASSERT_EQ(tested, _double_s);
+}
+
+TEST_F(MatrixIntTest, TransposeTest) {
+  Matrix<int> transposed = Matrix<int>({{1, 4}, {2, 5}, {3, 6}});
+  ASSERT_EQ(tested.T(), transposed);
+}
+
+TEST_F(MatrixIntTest, CallTest) {
+  ASSERT_EQ(tested(0,0), 1);
+  try {
+    auto test = tested(5,5);
+    FAIL() << "Expected std::out_of_range :1";
+  }
+  catch (std::out_of_range const &err) {
+    EXPECT_EQ(err.what(), std::string("Index out of range"));
+  }
+  catch (...) {
+    FAIL() << "Expected std::out_of_range :2";
+  }
+}
+
+TEST_F(MatrixIntTest, MultiplyTest) {
+  Matrix<int> matrix = Matrix<int>({{1,2,3}});
+  Matrix<int> matrixT = matrix.T();
+  Matrix<int> resutl = Matrix<int>({{14}});
+  Matrix<int> result2 = Matrix<int>({{1, 2, 3}, {2, 4, 6}, {3, 6, 9}});
+  ASSERT_EQ(matrix%matrixT, resutl);
+  ASSERT_EQ(matrixT%matrix, result2);
+}
+
 int main(int argc, char *argv[])
 {
   ::testing::InitGoogleTest(&argc, argv);
